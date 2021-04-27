5fdfa35a810c71544282339542f0aa08baebd605
mukuro557 <6131302005@lamduan.mfu.ac.th>
2021-04-28 03:34:20 +0700

    แก้ db

--- web/karenpro/karen/models.py
+++ web/karenpro/karen/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class user(models.Model):
-    User_name = models.CharField(max_length = 20)
-    User_password = models.CharField(max_length = 50)
-
 class word(models.Model):
     Word_th = models.CharField(max_length = 100)
     Sound = models.CharField(max_length = 100)
+
 class questions(models.Model):
     Question = models.CharField(max_length = 100)
+    Sound = models.CharField(max_length = 100)
     
 class choice(models.Model):
     Choice = models.CharField(max_length = 50)
     Icon = models.CharField(max_length = 60)
+    Sound = models.CharField(max_length = 100)
     Question_id = models.IntegerField()
 
