439d833e0a21fe6d15fb969e0f334014ea47af80
mukuro557 <6131302005@lamduan.mfu.ac.th>
2021-09-15 17:17:53 +0700

    เชื่อม serv

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'dart:async';
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:highlight_text/highlight_text.dart';
