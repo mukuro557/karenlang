8fd1e526b7dcd13f47b6f19e9f0e0502c88f0816
mukuro557 <6131302005@lamduan.mfu.ac.th>
2021-11-11 01:28:56 +0700

    แก้ editword

--- web/karenpro/karen/views.py
+++ web/karenpro/karen/views.py
@@ -1,5 +1,9 @@
+from rest_framework import status, generics, filters
+from rest_framework.response import Response
+from .serializer import getquestion
+from rest_framework.decorators import api_view
 from django.shortcuts import render, redirect
-from .models import word 
+from .models import word
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import word, questions, choice, usedquestion
@@ -18,32 +22,41 @@ dataSet = [
 
 ]
 
+
 def login(request):
     return render(request, 'login.html')
 
+
 def dashboad(request):
     alldata = []
-    data = usedquestion.objects.all().values('wordque','type','miss').annotate(total=Count('wordque')).order_by('-total')
-    countword = word.objects.all().values("Word_th").annotate(totals=Count('Word_th')).order_by('totals')
-    countques = questions.objects.all().values("Question").annotate(totalss=Count('Question')).order_by('totalss')
+    data = usedquestion.objects.all().values('wordque', 'type', 'miss').annotate(
+        total=Count('wordque')).order_by('-total')
+    countword = word.objects.all().values("Word_th").annotate(
+        totals=Count('Word_th')).order_by('totals')
+    countques = questions.objects.all().values("Question").annotate(
+        totalss=Count('Question')).order_by('totalss')
     for i in data:
-        alldata.append([i,countword,countques])
-    
-    return render(request, 'dashboad.html' , {'allword': alldata})
+        alldata.append([i, countword, countques])
+
+    return render(request, 'dashboad.html', {'allword': alldata})
 
 # @login_required(login_url="/")
+
+
 def mainpage(request):
     data = word.objects.all()
 
     return render(request, 'mainpage.html', {'allword': data})
 
 # @login_required(login_url="/")
+
+
 def addanswer(request):
     dataSet.clear()
     ques = questions.objects.all()
     for i in ques:
         num = 0
-        cho = choice.objects.all().filter(Question_id = i.pk)
+        cho = choice.objects.all().filter(Question_id=i.pk)
         allchoice = ''
         for j in cho:
 
@@ -56,6 +69,7 @@ def addanswer(request):
                         'Sound': i.Sound, 'choice': allchoice, 'type': i.Type})
         print(dataSet)
     return render(request, 'addanswer.html', {'allword': dataSet})
+
 
 def username_pass(request):
     username = request.POST['username']
@@ -72,9 +86,12 @@ def username_pass(request):
         return render(request, 'login.html', {'user': 'nouser'})
 
 # @login_required(login_url="/")
+
+
 def logout(request):
     auth.logout(request)
     return redirect('/')
+
 
 def adduser(request):
     username = 'admin'
@@ -86,10 +103,12 @@ def adduser(request):
     user.save()
     return render(request, 'test.html')
 
+
 def delete(request, pk):
     word.objects.filter(pk=pk).delete()
     data = word.objects.all()
     return render(request, 'mainpage.html', {'allword': data})
+
 
 def addword(request):
     data = word.objects.all()
@@ -113,9 +132,10 @@ def addword(request):
         data = word.objects.all()
         return render(request, 'mainpage.html', {'allword': data})
 
+
 def editword(request, pk):
 
-    if request.method == 'POST' and request.FILES['myfile'] and request.POST['wordth'] and request.POST['answer'] and request.FILES['ansfile1']:
+    if request.method == 'POST' and request.FILES['myfile'] and request.POST['wordth']:
         word_th = request.POST['wordth']
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
@@ -125,16 +145,17 @@ def editword(request, pk):
         word.objects.filter(pk=pk).update(
             Word_th=word_th, Sound=filename)
         data = word.objects.all()
-        return render(request, 'addanswer.html', {'allword': data})
+        return render(request, 'mainpage.html', {'allword': data})
 
     else:
         data = word.objects.all()
-        return render(request, 'addanswer.html', {'allword': data})
+        return render(request, 'mainpage.html', {'allword': data})
 
-def addquestion(request, number , type):
-    
-    if request.method == 'POST' and request.FILES['myfile'] and request.POST['wordth'] :
-        
+
+def addquestion(request, number, type):
+
+    if request.method == 'POST' and request.FILES['myfile'] and request.POST['wordth']:
+
         word_th = request.POST['wordth']
         mysound = request.FILES['myfile']
         fs = FileSystemStorage()
@@ -149,11 +170,11 @@ def addquestion(request, number , type):
 
         else:
             question_thai = questions.objects.create(
-                Question=word_th, Sound=filenameQ,Type = type)
+                Question=word_th, Sound=filenameQ, Type=type)
             question_thai.save()
             data = questions.objects.all()
-            
-            if type == 3 :
+
+            if type == 3:
                 for i in range(1, number+1):
                     findimg = request.FILES['ansfile%s' % i]
                     findicon = request.FILES['icon%s' % i]
@@ -166,29 +187,31 @@ def addquestion(request, number , type):
                     choicework.save()
 
                 return redirect('/addquestion')
-            elif type == 2: 
+            elif type == 2:
                 choicef = request.POST['answer%s' % 1]
                 findimg = request.FILES['ansfile%s' % 1]
                 filenameC = fs.save(findimg.name, findimg)
                 choicework = choice.objects.create(
-                        Choice=choicef, Question_id=question_thai.id, Sound=filenameC)
+                    Choice=choicef, Question_id=question_thai.id, Sound=filenameC)
                 choicework.save()
                 choicef = request.POST['answer%s' % 2]
                 findimg = request.FILES['ansfile%s' % 2]
                 filenameC = fs.save(findimg.name, findimg)
                 choicework = choice.objects.create(
-                        Choice=choicef, Question_id=question_thai.id, Sound=filenameC)
+                    Choice=choicef, Question_id=question_thai.id, Sound=filenameC)
                 choicework.save()
                 return redirect('/addquestion')
             else:
                 return redirect('/addquestion')
     return redirect('/addquestion')
 
+
 def deleteques(request, id):
     questions.objects.filter(pk=id).delete()
     choice.objects.filter(Question_id=id).delete()
 
     return redirect('/addquestion')
+
 
 def editques(request, id, number):
     if request.method == 'POST':
@@ -212,41 +235,37 @@ def editques(request, id, number):
 
     return redirect('/addquestion')
 
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status , generics, filters
-from .serializer import getquestion
-import spacy
 
-spacy.prefer_gpu()
 class cutkum(generics.ListCreateAPIView):
-    
-    search_field =['Question']
+
+    search_field = ['Question']
     all_athletes = questions.objects.all()
     filter_backends = (filters.SearchFilter,)
-    serializer_class= getquestion
+    serializer_class = getquestion
     serializer = getquestion(all_athletes, many=True)
     # sound = questions.objects.filter(Question=id)
+
     def get_queryset(self):
 
         word = self.request.query_params.get('word')
         proc = word_tokenize(word, engine='newmm')
         print(proc)
 
-def get_queryset(request,word):
 
+def get_queryset(request, word):
 
-    sound = questions.objects.all().filter(Question= word)
-    for j in sound :
+    sound = questions.objects.all().filter(Question=word)
+    for j in sound:
         print(j.Sound)
     return HttpResponse(sound[0].Sound, content_type='application/json')
 
-def get_question(request,word):
-    
-    if questions.objects.filter(Question= word):
-        ques = questions.objects.all().filter(Question= word)
+
+def get_question(request, word):
+
+    if questions.objects.filter(Question=word):
+        ques = questions.objects.all().filter(Question=word)
         data = []
-        data.append([ques[0].id,ques[0].Type,ques[0].Sound],)
+        data.append([ques[0].id, ques[0].Type, ques[0].Sound],)
         list_to_json_array = json.dumps(data)
         quest = usedquestion.objects.create(
             wordque=word, type=1, miss=0)
@@ -255,7 +274,7 @@ def get_question(request,word):
         return HttpResponse(list_to_json_array)
 
     else:
-        
+
         data = []
         data.append([''])
         # y = json.loads(ques)
@@ -266,44 +285,50 @@ def get_question(request,word):
         print(data)
         return HttpResponse(list_to_json_array)
 
+
 def get_allquestion(request):
 
     ques = questions.objects.all()
     data = []
-    for j in ques :
+    for j in ques:
         data.append([j.Question])
     print(data)
     list_to_json_array = json.dumps(data)
     return HttpResponse(list_to_json_array)
 
-def get_setanswer(request,id):
-    ques = choice.objects.all().filter(Question_id= id)
+
+def get_setanswer(request, id):
+    ques = choice.objects.all().filter(Question_id=id)
     data = []
-    for j in ques :
-        data.append([j.Choice,j.Icon,j.Sound])
+    for j in ques:
+        data.append([j.Choice, j.Icon, j.Sound])
     # y = json.loads(ques)
-    
+
     list_to_json_array = json.dumps(data)
     return HttpResponse(list_to_json_array)
 
+
 def get_recommen(request):
-    questt = usedquestion.objects.all().filter(miss= 0).values('wordque','type','miss').annotate(total=Count('wordque')).order_by('-total')
+    questt = usedquestion.objects.all().filter(miss=0).values(
+        'wordque', 'type', 'miss').annotate(total=Count('wordque')).order_by('-total')
     data = []
-    for j in questt :
+    for j in questt:
         data.append(j)
     # y = json.loads(ques)
-    
+
     list_to_json_array = json.dumps(data)
     print(list_to_json_array)
     return HttpResponse(list_to_json_array)
 
+
 def get_history(request):
-    questt = usedquestion.objects.all().filter(miss= 0).values('wordque','type','miss','id').distinct().order_by('-id')
+    questt = usedquestion.objects.all().filter(miss=0).values(
+        'wordque', 'type', 'miss', 'id').distinct().order_by('-id')
     data = []
-    for j in questt :
+    for j in questt:
         data.append(j)
     # y = json.loads(ques)
-    
+
     list_to_json_array = json.dumps(data)
     print(list_to_json_array)
     return HttpResponse(list_to_json_array)
